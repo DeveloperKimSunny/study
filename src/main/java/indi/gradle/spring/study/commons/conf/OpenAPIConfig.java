1c0d8df696015a0c45f89ce4bac368f1a090982e
sungookim <smileman.kim@gmail.com>
2022-12-26 16:59:04 +0900

    springdoc bearer 추가

--- src/main/java/indi/gradle/spring/study/commons/conf/OpenAPIConfig.java
+++ src/main/java/indi/gradle/spring/study/commons/conf/OpenAPIConfig.java
@@ -1,8 +1,11 @@
 package indi.gradle.spring.study.commons.conf;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +21,15 @@ public class OpenAPIConfig {
                 .description("API에 대한 설명 부분");
 
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth", new SecurityScheme().name("bearerAuth")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(info)
+                ;
     }
 }
